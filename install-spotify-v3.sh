e7ffb7b7473fa988ea1d548cc4d94f3dc97db2f6
Hendrik Schmitz <hendrik.schmitz03.sh@gmail.com>
2019-03-21 20:30:20 +0100

    Update install-spotify-v3.sh

--- install-spotify-v3.sh
+++ install-spotify-v3.sh
@@ -12,18 +12,15 @@
 ##################################################################################################################
 
 
-
-# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-
-# 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-# 3. Update list of available packages
+# 1. Update list of available packages
 sudo apt-get update
 
-# 4. Install Spotify
-sudo apt-get install spotify-client
+# 2. Install snap
+sudo apt-get install snapd
+
+# 3. Install spotify
+snap install spotify
+
 
 
 
